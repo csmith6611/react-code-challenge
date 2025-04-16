3f94c067f0723be315a6cc9b4d9c7957c04c1ecb
csmith6611 <pc.smith.dev@gmail.com>
2025-04-16 10:13:33 -0400

    WIP - create hydration scroller component

--- src/components/scroller/NewsFeedScroller.tsx
+++ src/components/scroller/NewsFeedScroller.tsx
@@ -1,169 +1,28 @@
-import React, { JSX, useCallback, useEffect, useMemo } from "react";
+import React, { JSX, ReactNode, useCallback, useEffect, useMemo } from "react";
 import { useHackerNewsFeed } from "../../hooks/useHackerNewsFeed";
 import { Card } from "../display/Card";
 
 export function NewsFeedScroller() {
-  const [loaded_page, set_loaded_page] = React.useState(1);
-  const [page_size, set_page_size] = React.useState(20);
+  const [page_size, set_page_size] = React.useState(10);
 
-  const [visible_page_tuple, set_visible_page_tuple] = React.useState([-1, 1]);
-
-  const {
-    posts: data,
-    error,
-    loading,
-    posts_fetcher,
-  } = useHackerNewsFeed(0, 20);
-  const [cached_data, set_cached_data] = React.useState(data);
-
-  const bottom_scroll_ref = React.useRef<HTMLDivElement>(null);
-  const top_scroll_ref = React.useRef<HTMLDivElement>(null);
-
-  const page_components_array: JSX.Element[] = useMemo(() => {
-    const components: JSX.Element[] = [];
-
-    const lower_hydration_bound = Math.max(
-      visible_page_tuple[0] * page_size,
-      0
-    );
-    const upper_hydration_bound = visible_page_tuple[1] * page_size - 1;
-
-    for (let i = 0; i < loaded_page * page_size; i++) {
-      if (i < lower_hydration_bound) {
-        components.push(
-          <div className="h-48" key={i}>
-            UNRENDERED
-          </div>
-        );
-        continue;
-      }
-
-      if (i > upper_hydration_bound) {
-        components.push(
-          <div className="h-48" key={i}>
-            UNRENDERED
-          </div>
-        );
-
-        continue;
-      }
-
-      if (i === lower_hydration_bound) {
-        components.push(
-          <div className="h-2" key={i + "ref"} ref={top_scroll_ref}>
-            TOP
-          </div>
-        );
-      }
-
-      if (i === upper_hydration_bound) {
-        components.push(
-          <div className="h-2" key={i + "ref"} ref={bottom_scroll_ref}>
-            Bottom
-          </div>
-        );
-      }
-
-      if (cached_data[i]) {
-        components.push(
-          <Card
-            key={i}
-            title={cached_data[i].title ?? "ERROR"}
-            author={cached_data[i].author ?? "ERROR"}
-            points={cached_data[i].points ?? 0}
-            time={cached_data[i].updated_at.toString() ?? "ERROR"}
-            url={cached_data[i].url ?? "ERROR"}
-          />
-        );
-      } else {
-        components.push(
-          <div className="h-48" key={i}>
-            SKELETON LOADER
-          </div>
-        );
-      }
-    }
-
-    return components;
-  }, [cached_data, loaded_page, page_size, visible_page_tuple]);
-
-  useEffect(() => {
-    if (data.length > 0) {
-      set_cached_data((prev) => {
-        //filter out the posts that are already in the cache
-
-        const new_posts = data.filter(
-          (post) => !prev.some((p) => p.objectID === post.objectID)
-        );
-        return [...prev, ...new_posts];
-      });
-    }
-  }, [data]);
-
-  const handle_top_scroll = useCallback(() => {
-    if (visible_page_tuple[0] > 0) {
-      set_visible_page_tuple((prev) => [prev[0] - 1, prev[1] - 1]);
-    }
-  }, [visible_page_tuple]);
-
-  useIntersectionObserver(top_scroll_ref, (entry) => {
-    if (entry.isIntersecting) {
-      handle_top_scroll();
-    }
-  });
-
-  const handle_bottom_scroll = useCallback(async () => {
-    console.log("Bottom scroll handler called");
-    if (visible_page_tuple[1] === loaded_page) {
-      set_visible_page_tuple((prev) => [prev[0] + 1, prev[1] + 1]);
-      set_loaded_page(loaded_page + 1);
-      //fetch more data here
-
-      const posts = await posts_fetcher(page_size, loaded_page + 1);
-
-      set_cached_data((prev) => {
-        const new_posts = posts.filter(
-          (post) => !prev.some((p) => p.objectID === post.objectID)
-        );
-        return [...prev, ...new_posts];
-      });
-    } else {
-      set_visible_page_tuple((prev) => [prev[0] + 1, prev[1] + 1]);
-    }
-  }, [loaded_page, visible_page_tuple, posts_fetcher, page_size]);
-
-  useIntersectionObserver(bottom_scroll_ref, (entry) => {
-    if (entry.isIntersecting) {
-      handle_bottom_scroll();
-    }
-  });
-
-  useIntersectionObserver(top_scroll_ref, (entry) => {
-    if (entry.isIntersecting) {
-      handle_top_scroll();
-    }
-  });
-
-  console.log(
-    bottom_scroll_ref.current,
-    top_scroll_ref.current,
-    visible_page_tuple,
-    loaded_page
-  );
+  const { posts_fetcher } = useHackerNewsFeed(1, page_size);
 
   return (
-    <main className="flex flex-col gap-4 w-3/4 mx-auto mt-4 justify-center items-center">
-      {page_components_array &&
-        page_components_array?.map((component, index) => {
-          return <React.Fragment key={index}>{component}</React.Fragment>;
-        })}
-
-      {loading && (
-        <div className="flex justify-center items-center w-full h-48">
-          <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-gray-900"></div>
-        </div>
-      )}
-    </main>
+    <div className="flex flex-col gap-4 items-center justify-center w-full">
+      <HydrationInfiniteScroller
+        DisplayComponent={Card}
+        fetch_callback={posts_fetcher}
+        data_key="objectID"
+        page_size={page_size}
+        pages_shown={1}
+        custom_displays={{
+          Loader: <div>Loading...</div>,
+          Error: <div>Error</div>,
+          Skeleton: <div className="h-48 bg-gray-200 animate-pulse"></div>,
+          Unrendered: <div className="h-48 bg-gray-200"></div>,
+        }}
+      />
+    </div>
   );
 }
 
@@ -200,4 +59,188 @@ function useIntersectionObserver(
       }
     };
   }, [ref, callback]); // Re-run the effect if `ref` or `callback` changes
+}
+
+//turn this into a generic component
+
+function HydrationInfiniteScroller<Data, DisplayProps>(props: {
+  DisplayComponent: React.FC<DisplayProps>;
+  fetch_callback: (page_size: number, page: number) => Promise<Data[]>;
+  data_key: string;
+  page_size: number;
+  pages_shown: number;
+  initial_data?: Data[];
+  custom_displays: {
+    Loader: ReactNode;
+    Error: ReactNode;
+    Skeleton: ReactNode;
+    Unrendered: ReactNode;
+  };
+}) {
+  const {
+    fetch_callback: data_fetcher,
+    page_size,
+    data_key,
+    initial_data,
+    DisplayComponent,
+    custom_displays: { Loader, Error, Skeleton, Unrendered },
+  } = props;
+
+  const [loaded_page, set_loaded_page] = React.useState(1);
+
+  const [cached_data, set_cached_data] = React.useState(initial_data ?? []);
+
+  const [loading, set_loading] = React.useState(false);
+
+  const [visible_page_tuple, set_visible_page_tuple] = React.useState([
+    -1,
+    props.pages_shown ?? 1,
+  ]);
+
+  const bottom_scroll_ref = React.useRef<HTMLDivElement>(null);
+  const top_scroll_ref = React.useRef<HTMLDivElement>(null);
+
+  const page_components_array: JSX.Element[] = useMemo(() => {
+    if (!cached_data) return [];
+
+    const components: JSX.Element[] = [];
+
+    const lower_hydration_bound = Math.max(
+      visible_page_tuple[0] * page_size,
+      0
+    );
+    const upper_hydration_bound = visible_page_tuple[1] * page_size - 1;
+
+    for (let i = 0; i < loaded_page * page_size; i++) {
+      if (i < lower_hydration_bound) {
+        components.push(
+          <div className="h-48" key={i}>
+            {Unrendered}
+          </div>
+        );
+        continue;
+      }
+
+      if (i > upper_hydration_bound) {
+        components.push(
+          <div className="h-48" key={i}>
+            {Unrendered}
+          </div>
+        );
+
+        continue;
+      }
+
+      if (i === lower_hydration_bound) {
+        components.push(
+          <div className="h-2" key={i + "ref"} ref={top_scroll_ref}>
+            TOP
+          </div>
+        );
+      }
+
+      if (i === upper_hydration_bound) {
+        components.push(
+          <div className="h-2" key={i + "ref"} ref={bottom_scroll_ref}>
+            Bottom
+          </div>
+        );
+      }
+
+      if (cached_data[i]) {
+        components.push(
+          <DisplayComponent
+            key={i}
+            {...(cached_data[i] as unknown as DisplayProps)}
+          />
+        );
+      } else {
+        components.push(
+          <div className="h-48" key={i}>
+            {Skeleton}
+          </div>
+        );
+      }
+    }
+
+    return components;
+  }, [
+    DisplayComponent,
+    Skeleton,
+    Unrendered,
+    cached_data,
+    loaded_page,
+    page_size,
+    visible_page_tuple,
+  ]);
+
+  useEffect(() => {
+    const get_data = async () => {
+      if (cached_data.length === 0) {
+        const posts = await data_fetcher(page_size, loaded_page);
+        set_cached_data((prev) => {
+          //filter out the posts that are already in the cache
+
+          const new_posts = posts.filter(
+            (post) => !prev.some((p) => p[data_key] === post[data_key])
+          );
+          return [...prev, ...new_posts];
+        });
+      }
+    };
+
+    get_data();
+  }, [cached_data, data_fetcher, data_key, loaded_page, page_size]);
+
+  const handle_top_scroll = useCallback(() => {
+    if (visible_page_tuple[0] > 0) {
+      set_visible_page_tuple((prev) => [prev[0] - 1, prev[1] - 1]);
+    }
+  }, [visible_page_tuple]);
+
+  useIntersectionObserver(top_scroll_ref, (entry) => {
+    if (entry.isIntersecting) {
+      handle_top_scroll();
+    }
+  });
+
+  const handle_bottom_scroll = useCallback(async () => {
+    console.log("Bottom scroll handler called");
+    if (visible_page_tuple[1] === loaded_page) {
+      set_visible_page_tuple((prev) => [prev[0] + 1, prev[1] + 1]);
+      set_loaded_page(loaded_page + 1);
+
+      set_loading(true);
+      //fetch more data here
+
+      const posts = await data_fetcher(page_size, loaded_page + 1);
+
+      set_cached_data((prev) => {
+        const new_posts = posts.filter(
+          (post) => !prev.some((p) => p[data_key] === post[data_key])
+        );
+        set_loading(false);
+        return [...prev, ...new_posts];
+      });
+    } else {
+      set_visible_page_tuple((prev) => [prev[0] + 1, prev[1] + 1]);
+    }
+  }, [visible_page_tuple, loaded_page, data_fetcher, page_size, data_key]);
+
+  useIntersectionObserver(bottom_scroll_ref, (entry) => {
+    if (entry.isIntersecting) {
+      handle_bottom_scroll();
+    }
+  });
+
+  return (
+    <>
+      {page_components_array &&
+        page_components_array?.map((component, index) => {
+          return <React.Fragment key={index}>{component}</React.Fragment>;
+        })}
+
+      {loading && <>{Loader}</>}
+    </>
+  );
 }
